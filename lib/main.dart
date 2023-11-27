b37f10d892906968b070f45b25ae648011f30d5b
Shohjaxon <118268074+Shohjaxon-098@users.noreply.github.com>
2023-11-27 19:57:51 +0500

    FIRST COMMIT

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:music_player/audio_helpers/page_manager.dart';
+import 'package:music_player/audio_helpers/service_locator.dart';
+import 'package:music_player/common/color_extension.dart';
+import 'package:music_player/view/splash_view.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupServiceLocator();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getIt<PageManager>().init();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    getIt<PageManager>().dispose();
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Music Player',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "Circular Std",
+        scaffoldBackgroundColor: TColor.bg,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: TColor.primaryText,
+              displayColor: TColor.primaryText,
+            ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: TColor.primary,
+        ),
+        useMaterial3: false,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
---
+++ lib/view_model/splash_view_model.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:music_player/view/main_tabview/main_tabview.dart';
+
+class SplashViewMode extends GetxController {
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void loadView() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Get.to(() => const MainTabView());
+  }
+
+  void openDrawer() {
+    scaffoldKey.currentState?.openDrawer();
+  }
+
+  void closeDrawer() {
+    scaffoldKey.currentState?.closeDrawer();
+  }
+}
